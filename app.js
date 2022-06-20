464e6f07711aa52eafb9a06d67467d76aef27990
sergiolaulhe <sergiolaulhe@hotmail.com>
2022-06-20 10:56:48 -0300

    actualizacion de archivos

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.listen(3000, () =>
     console.log('Servidor en linea en puerto 3000'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
